b61d0d1654cec22fc9914e4ade9cca2d599dd1c7
Fatih-Tarim <fatihtrm99@gmail.com>
2022-09-22 15:20:29 +0300

    Templates

--- blogapp/blog/views.py
+++ blogapp/blog/views.py
@@ -5,10 +5,12 @@ from http.client import HTTPResponse
 from django.shortcuts import render, HttpResponse
 
 def index(request):
-    return HttpResponse("hoempage")
+    return render(request, "blog/index.html")
 
 def blogs(request):
-    return HttpResponse("Blogs")
+    return render(request, "blog/blogs.html")
 
 def blog_details(request, id):
-    return HttpResponse("blog details:"+str(id))
+    return render(request, "blog/blog-details.html",{
+        "id":id 
+    })
