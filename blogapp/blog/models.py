0ddcf8700bb9f1ca5a511081bb2349d8ab7a4471
Fatih-Tarim <fatihtrm99@gmail.com>
2022-09-28 22:22:30 +0300

    Admin Models

--- blogapp/blog/models.py
+++ blogapp/blog/models.py
@@ -2,11 +2,19 @@ from django.db import models
 
 
 class Blog(models.Model):
+    def __str__(self):
+        return f"{self.title}"
+
     title = models.CharField(max_length=200)
     image = models.CharField(max_length=50, null=True)
     description = models.TextField()
     is_active = models.BooleanField()
     is_home = models.BooleanField(default=False)
 
+    
+
 class Category(models.Model):
+    def __str__(self):
+        return self.name
+
     name = models.CharField(max_length=155)
--- blogapp/blog/views.py
+++ blogapp/blog/views.py
@@ -1,47 +1,23 @@
 from http.client import HTTPResponse
 from msilib.schema import LockPermissions
 from django.shortcuts import render, HttpResponse
+from blog.models import Blog
 
 data = {
-    "blogs": [
-        {
-            "id": 1,
-            "title": "Kurs1",
-            "image": "1.png",
-            "is_active": True,
-            "is_home": True,
-            "description": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidemminus nulla nobis odit blanditiis numquam!"
-        },
-        {
-            "id": 2,
-            "title": "Kurs2",
-            "image": "1.png",
-            "is_active": True,
-            "is_home": True,
-            "description": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidemminus nulla nobis odit blanditiis numquam!"
-        },
-        {
-            "id": 3,
-            "title": "Kurs3",
-            "image": "1.png",
-            "is_active": True,
-            "is_home": False,
-            "description": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidemminus nulla nobis odit blanditiis numquam!"
-        },
-    ]
+
 }
 
 
 def index(request):
     context = {
-        "blogs": data["blogs"]
+        "blogs": Blog.objects.filter(is_home=True, is_active=True)
     }
     return render(request, "blog/index.html", context)
 
 
 def blogs(request):
     context = {
-        "blogs": data["blogs"]
+        "blogs": Blog.objects.filter(is_active=True)
     }
     return render(request, "blog/blogs.html", context)
 
@@ -53,9 +29,11 @@ def blog_details(request, id):
     #     if blog["id"] == id:
     #         selectedBlog = blog
 
-    blogs = data["blogs"]
-    selectedBlog = [blog for blog in blogs if blog["id"]==id][0]
+    # blogs = data["blogs"]
+    # selectedBlog = [blog for blog in blogs if blog["id"]==id][0]
+
+    blog = Blog.objects.get(id=id)
 
     return render(request, "blog/blog-details.html", {
-        "blog": selectedBlog
+        "blog": blog
     })
