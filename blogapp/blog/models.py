67e82ec3aaf0a5e45c37bc4f3dca37fee15fc716
Fatih-Tarim <fatihtrm99@gmail.com>
2022-09-29 18:21:23 +0300

    SlugField

--- blogapp/blog/models.py
+++ blogapp/blog/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.utils.text import slugify
 
 class Blog(models.Model):
     def __str__(self):
@@ -10,11 +10,21 @@ class Blog(models.Model):
     description = models.TextField()
     is_active = models.BooleanField()
     is_home = models.BooleanField(default=False)
+    slug = models.SlugField(null=False, blank=True, unique=True, db_index=True, editable=False)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
     
-
+# ...com/Category/category-name
 class Category(models.Model):
     def __str__(self):
         return self.name
 
     name = models.CharField(max_length=155)
+    #slug = models.SlugField(null=True, unique=True, db_index=True, editable=False, blank=True)
+
+    # def save(self, *args, **kwargs):
+    #     self.slug = slugify(self.name)
+    #     super().save(*args, **kwargs)
